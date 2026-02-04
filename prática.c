ed6ca1cca8bd9c8c6759d8563ec491014d394d0e
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-02-04 07:20:47 +0000

    pratica

---
+++ prática.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 18;
+
+    printf("Digite sua idade!!!\n");
+    
+    if (idade >= 18){
+
+        printf("Voce é maior de idade!!!\n");
+    } else 
+{
+    printf("Voce é menor de idade!!!\n");
+    }
+
+
+
+
+return 0;
+
+
+
+}
